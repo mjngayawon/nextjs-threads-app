d8d8258c7802b6d28a7c33981e5f3c60349e0d2d
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2025-10-01 15:23:30 +0800

    Thread page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,14 +1,16 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null;
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
 
   const userData = {
     id: user?.id,
-    objectId: userInfo?._id,
+    objectId: userInfo?._id?.toString(),
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || "",
     bio: userInfo?.bio || "",
