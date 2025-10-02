3be6107ddb4e2916cb10426aee3cd9129bb80aef
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2025-10-02 14:33:53 +0800

    Profile Page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,22 +4,30 @@ import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 
 function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { userId } = useAuth();
+
   return (
-    <section className="custom-srcollbar leftsidebar">
+    <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
-          const router = useRouter();
-          const pathname = usePathname();
+          const route =
+            link.route === "/profile" && userId
+              ? `/profile/${userId}`
+              : link.route;
+
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
 
           return (
             <Link
-              href={link.route}
+              href={route}
               key={link.label}
               className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
             >
