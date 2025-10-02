268b280da4f581c55f9f0725671a7dd5fba9dbc2
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2025-10-02 16:36:06 +0800

    Activity

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,12 +1,14 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user?.id,
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -60,7 +60,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     if (hasImageChanged) {
       const imgRes = await startUpload(files);
 
-      if (imgRes && imgRes[0].url) {
+      if (imgRes && imgRes?.[0]?.url) {
         values.profile_photo = imgRes[0].url;
       }
     }
